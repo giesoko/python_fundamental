4dca5a93f5d99b2bef3ad06da4a73cf22fb0c6af
giesoko <pramudya.soko@gmail.com>
2022-02-11 22:36:57 +0700

    del command

--- basic_syntax_datatype_list.py
+++ basic_syntax_datatype_list.py
@@ -1,4 +1,4 @@
-books = ['Seven Habits', 'How to influence people', 'First Things First']
+books = ['Seven Habits', 'How to influence people', 'First Things First', 'Predictably Irrational']
 print(books)
 print('\nUsing loop to print:')
 for i in books:
@@ -21,7 +21,7 @@ for i in range(0,len(books)):
 
 print('\nModifying an element in a list:')
 print('Modifying the first element, from Seven Habits to Harry Potter')
-books = ['Seven Habits', 'How to influence people', 'First Things First']
+books = ['Seven Habits', 'How to influence people', 'First Things First', 'Predictably Irrational']
 books[0] = 'Harry Potter'
 for i in range(0,len(books)):
     print(books[i])
@@ -41,3 +41,36 @@ for i in range(0,len(books)):
     print(books[i])
 last_book = books.pop()
 print(f'\nI took {last_book} from the Books list')
+print(f'\nI put the {last_book} back to the Books list')
+books.append(last_book)
+for i in range(0,len(books)):
+    print(books[i])
+
+print('\nDel command')
+del books[0]
+for i in range(0,len(books)):
+    print(books[i])
+
+books = ['Seven Habits', 'How to influence people', 'First Things First', 'Predictably Irrational']
+print('\nDel command with list comprehension')
+del books[:]
+for i in range(0,len(books)):
+    print(books[i])
+
+books = ['Seven Habits', 'How to influence people', 'First Things First', 'Predictably Irrational']
+print('\nDel command with list comprehension [start:count]')
+del books[0:2]
+for i in range(0,len(books)):
+    print(books[i])
+
+books = ['Seven Habits', 'How to influence people', 'First Things First', 'Predictably Irrational']
+print('\nDel command with list comprehension [start:-count]')
+del books[0:-1]
+for i in range(0,len(books)):
+    print(books[i])
+
+books = ['Seven Habits', 'How to influence people', 'First Things First', 'Predictably Irrational']
+print('\nDel command with list comprehension [start:count:step]') #blank [count] means up to the last element
+del books[0::2]
+for i in range(0,len(books)):
+    print(books[i])
